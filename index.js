15e619df041b151b8942ef26aa56f9201384ee05
Joshua <infiniteloop311@gmail.com>
2019-06-18 18:19:33 -0400

    testing team activity 09

--- index.js
+++ index.js
@@ -1,10 +1,70 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const app = express()
+const PORT = process.env.PORT || 8888
 
+/*
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+*/
+
+// tell it to use the public directory as one where static files live
+app.use(express.static(path.join(__dirname, '/public')))
+
+// views is directory for all template files
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.sendFile('form.html', {
+        root: path.join(__dirname, './views/pages')
+    })
+})
+// set up a rule that says requests to "/math" should be handled by the
+// handleMath function below
+app.get('/math', handleMath)
+app.get('/math_service', handleMath)
+
+// start the server listening
+app.listen(PORT, function() { console.log('Node app is running on port', PORT); })
+
+function handleMath(request, response) {
+    const operation = request.query.operation
+	const operand1 = Number(request.query.operand1)
+	const operand2 = Number(request.query.operand2)
+
+	// TODO: Here we should check to make sure we have all the correct parameters
+
+	computeOperation(response, operation, operand1, operand2)
+}
+
+function computeOperation(response, op, left, right) {
+    op = op.toLowerCase();
+
+	let result = 0;
+
+	if (op == "add") {
+		result = left + right;
+	} else if (op == "subtract") {
+		result = left - right;		
+	} else if (op == "multiply") {
+		result = left * right;
+	} else if (op == "divide") {
+		result = left / right;
+	} else {
+		// It would be best here to redirect to an "unknown operation"
+		// error page or something similar.
+	}
+
+	// Set up a JSON object of the values we want to pass along to the EJS result page
+	const params = {operation: op, left: left, right: right, result: result};
+
+	// Render the response, using the EJS page "result.ejs" in the pages directory
+	// Makes sure to pass it the parameters we need.
+	response.render('pages/results', params);
+
+}

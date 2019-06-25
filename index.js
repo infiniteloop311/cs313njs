68f079b07dbc698cd16d49401981876706c41822
Joshua <infiniteloop311@gmail.com>
2019-06-25 17:56:04 -0400

    testing ajax form

--- index.js
+++ index.js
@@ -34,7 +34,8 @@ app.get('/', (req, res) => {
 
 app.get('/getPerson', function(req, res){
     console.log("Wrong Function!");
-    //res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+    //This line below does the same as the one below that
+    // res.json(["Tony","Lisa","Michael","Ginger","Food"]);
     res.send(["Tony","Lisa","Michael","Ginger","Food"]);
 });
 
